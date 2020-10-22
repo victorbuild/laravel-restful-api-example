cdbd2defb38e5021590a9c84f10a4e6952db83f5
victor <victor@langlangthing.com>
2020-10-22 23:45:55 +0800

    建立 type 資源

---
+++ app/Http/Controllers/TypeController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Type;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use Symfony\Component\HttpFoundation\Response;
+
+class TypeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $types = Type::get();
+
+        return response([
+            'data' => $types
+        ], Response::HTTP_OK);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            // 另外一種驗證的寫法，使用陣列傳入驗證關鍵字
+            'name' => [
+                'required',
+                'max:50',
+                // types資料表中name欄位資料是唯一值
+                Rule::unique('types', 'name')
+            ],
+            'sort' => 'nullable|integer',
+        ]);
+        // 如果沒有傳入sort欄位內容
+        if (!isset($request->sort)) {
+            // 找到目前資料表的排序欄位最大值
+            $max = Type::max('sort');
+            $request['sort'] = $max + 1; // 最大值加1寫入請求的資料中
+        }
+        $type = Type::create($request->all()); // 寫入資料庫
+
+        return response([
+            'data' => $type
+        ], Response::HTTP_CREATED);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Type  $type
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Type $type)
+    {
+        return response([
+            'data' => $type
+        ], Response::HTTP_OK);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Type  $type
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Type $type)
+    {
+        $this->validate($request, [
+            'name' => [
+                'max:50',
+                // 更新時排除自己的名稱後，檢查是否為唯一值
+                Rule::unique('types', 'name')->ignore($type->name, 'name')
+            ],
+            'sort' => 'nullable|integer',
+        ]);
+
+        $type->update($request->all());
+
+        return response([
+            'data' => $type
+        ], Response::HTTP_OK);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Type  $type
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Type $type)
+    {
+        $type->delete();
+        return response(null, Response::HTTP_NO_CONTENT);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AnimalController;
+use App\Http\Controllers\TypeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,3 +21,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::apiResource('animals', AnimalController::class);
+Route::apiResource('types', TypeController::class);
