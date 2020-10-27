a077736aa900d833bf0570c2717996018f24fd83
victor <victor@langlangthing.com>
2020-10-27 13:33:22 +0800

    移除UpdateAnimalRequest註解

--- app/Http/Requests/UpdateAnimalRequest.php
+++ app/Http/Requests/UpdateAnimalRequest.php
@@ -25,13 +25,12 @@ class UpdateAnimalRequest extends FormRequest
     {
         return [
             'type_id' => 'nullable|exists:types,id',
-            'name' => 'string|max:255',             // 文字類型最多255字元
-            // 允許null並且為日期格式
+            'name' => 'string|max:255',
             'birthday' => 'nullable|date',
-            'area' => 'nullable|string|max:255',    // 允許null或文字最多255字元
-            'fix' => 'boolean',                     // 若填寫必須是布林值
-            'description' => 'nullable|string',     // 允許null或文字
-            'personality' => 'nullable|string',     // 允許null或文字
+            'area' => 'nullable|string|max:255',
+            'fix' => 'boolean',
+            'description' => 'nullable|string',
+            'personality' => 'nullable|string',
         ];
     }
 }
